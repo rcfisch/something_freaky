905a6995feca582c7c328da2044f5da4b5c8f6fb
Autumn <snqwfall@gmail.com>
2025-05-10 16:10:09 -0400

    worked on basic player script

---
+++ something-freaky/Entities/Player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var move_dir : int = 0
+var speed : int = 2000
+var accel : int = 100
+var friction : float = 0.2 # tune this value to control friction strength
+var input_friction : float = 0.1
+var gravity : float = 20
+
+func _physics_process(delta: float) -> void:
+	move_dir = Input.get_axis("left", "right") # get input direction
+	velocity.x += (accel * move_dir)
+	
+	apply_friction(delta)
+	apply_gravity(delta)
+	move_and_slide()
+
+
+func apply_friction(delta):
+		if move_dir == 0:
+			velocity.x -= (velocity.x * friction)
+		else: 
+			velocity.x -= (velocity.x * input_friction)
+func apply_gravity(delta):
+	if !is_on_floor():
+		velocity.y += gravity
+	
