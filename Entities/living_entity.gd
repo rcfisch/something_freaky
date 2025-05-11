19b1ce11da8ac17a09906f0cb147bc7dfecf641d
Roland <117606042+rcfisch@users.noreply.github.com>
2025-05-10 23:53:36 -0400

    Finished living entity health script

--- Entities/living_entity.gd
+++ Entities/living_entity.gd
@@ -9,3 +9,29 @@ class_name living_entity
 @export var resistances : Array[damage_types]
 
 @onready var health : int = max_health
+
+# changes health by set amount, returns whether it killed the entity
+func change_health(change:int, types:Array[damage_types]=[])->bool:
+	#the mod variable
+	var mod:int = 1
+	#calculates modifications
+	for type in types:
+		if weaknesses.has(type):
+			mod = mod ** 0.8
+		if resistances.has(type):
+			mod = mod ** 1.2
+	#the actual health calculation
+	health -= change*mod
+	#if health at or below 0 trigger death
+	if (health <= 0):
+		trigger_death()
+		print("entity: " + code + "has died.")
+	#prevents health going above max health
+	elif(health > max_health):
+		health = max_health
+	return health <= 0 # returns whether it was a death blow
+
+#if you need to override this function just call super() at the end
+func trigger_death():
+	#destroys the enemy object
+	queue_free()
