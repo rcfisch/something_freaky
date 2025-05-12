679dabc6670e325b773cdb2423c0fd69ac9d9838
Autumn <snqwfall@gmail.com>
2025-05-12 01:36:27 -0400

    coded a celeste-esque dash

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -5,10 +5,18 @@ var accel : int = 400 # pixels/frame
 var air_accel : int = 400 # pixels/frame
 var friction : float = 0.2 # value 0-1 that controls the amount of the player's velocity that's removed per frame- 1 will stop immediately, 0 will accelerate never stop 
 var air_friction : float = 0.05 # read above
-var air_input_friction : float = 0.001 # read above
-var input_friction : float = 0.005 # read above
+var air_input_friction : float = 0.0002 # read above
+var input_friction : float = 0.001 # read above
 var max_fall_speed : int = 1600 * 4 # pixels/frame
 var max_walk_speed : int = 800 * 4 # pixels/frame
+# Dash
+var dash_velocity : int = 2000 * 4
+var dash_frames : int  = 12
+var special_dash_frames = 16
+var frames_since_dash : int
+var dash_direction : Vector2
+var wavedash_vel : int = 3000 * 4
+var facing : Vector2 = Vector2(1,1)
 # Jump
 @export var jump_height : float = 300 * 4 # pixels
 @export var jump_seconds_to_peak : float = 0.5
@@ -34,7 +42,11 @@ var afterimage_pos : Vector2
 
 
 func _physics_process(delta: float) -> void:
+	
 	move(delta) # handles movement and player input
+	get_facing()
+	animate()
+	dash()
 	handle_jump_frames() # handles coyote time and jump buffering
 	if is_on_floor() and jump_buffer_frames == 0: is_jumping = false
 	if Input.is_action_just_pressed("jump") and !is_jumping:
@@ -51,6 +63,9 @@ func _physics_process(delta: float) -> void:
 	speed_boost()
 	handle_afterimage()
 	move_and_slide() # built in function required for movement
+	dash_after_move_and_slide()
+func animate():
+	$StaticSprite.scale.x = -facing.x
 
 func move(delta):
 	move_dir = Input.get_axis("left", "right")
@@ -60,8 +75,14 @@ func move(delta):
 		accel_rate = accel
 	else:
 		accel_rate = air_accel
-	
-	velocity.x = approach(velocity.x, target_speed, accel_rate * delta * 60)
+	if frames_since_dash == 0:
+		velocity.x = approach(velocity.x, target_speed, accel_rate * delta * 60)
+func get_facing() -> Vector2:
+	if Input.get_axis("left","right") != 0:
+		facing.x = Input.get_axis("left","right")
+	if Input.get_axis("up","down") != 0:
+		facing.y = Input.get_axis("left","right")
+	return facing
 
 func approach(current: float, target: float, amount: float) -> float:
 	if current < target:
@@ -105,6 +126,13 @@ func jump():
 		velocity.y = jump_velocity
 		is_jumping = true
 		coyote_time = 0
+		# Cancel dash when jump happens
+		if frames_since_dash > 0:
+			frames_since_dash = 0
+			velocity.y = jump_velocity / 1.5
+			velocity.x = wavedash_vel * move_dir
+		else:
+			velocity.y = jump_velocity
 func handle_jump_frames():
 	if is_on_floor() and is_jumping == false:
 		coyote_time = coyote_frames
@@ -134,16 +162,52 @@ func print_stats():
 func speed_boost():
 	var ui_dir : Vector2
 	ui_dir = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity += Vector2(1000,1000) * ui_dir
+	velocity += Vector2(4000,4000) * ui_dir
 	
 func handle_afterimage():
 	if Input.is_action_just_pressed("afterimage"):
-		if !afterimage_cast: 
+		if !afterimage_cast:
 			afterimage_pos = self.position
 			afterimage_cast = !afterimage_cast
 		else:
 			self.position = afterimage_pos
 			afterimage_cast = !afterimage_cast
+			
+func dash():
+	if frames_since_dash <= dash_frames and frames_since_dash > 0:
+		if dash_direction.normalized().y == 1 and is_on_floor(): 
+			frames_since_dash = 0
+		else:
+			velocity.x += dash_velocity * dash_direction.normalized().x
+			velocity.y = dash_velocity * dash_direction.normalized().y
+			frames_since_dash += 1
+	elif frames_since_dash > dash_frames: 
+		# Clamp horizontal velocity only if the dash wasn't downward
+		if dash_direction.y <= 0 or is_on_floor():
+			velocity.x = clamp(velocity.x, -max_walk_speed, max_walk_speed)
+		if dash_direction.normalized().y < 0:
+			velocity.y = clamp(velocity.y, -max_walk_speed, max_walk_speed)
+		frames_since_dash = 0
+	if Input.is_action_just_pressed("dash"):
+		dash_direction = Input.get_vector("left","right","up","down")
+		if dash_direction == Vector2.ZERO:
+			dash_direction.x = facing.x
+		if abs(velocity.x) < max_walk_speed:
+			velocity.x = dash_velocity * dash_direction.normalized().x
+		else:
+			velocity.x += dash_velocity * dash_direction.normalized().x
+		velocity.y = dash_velocity * dash_direction.normalized().y
+		frames_since_dash += 1
+func dash_after_move_and_slide():
+	if Input.is_action_just_pressed("dash") and !abs(velocity.x - dash_velocity * dash_direction.normalized().x) < max_walk_speed:
+		velocity.x -= dash_velocity * dash_direction.normalized().x
+	if frames_since_dash <= dash_frames and frames_since_dash > 0:
+		if dash_direction.normalized().y == 0 and is_on_floor(): 
+			frames_since_dash = 0
+		else:
+			velocity.x -= dash_velocity * dash_direction.normalized().x
+	
+		
 func trigger_death():
 	
 	print("Player dead :(")
