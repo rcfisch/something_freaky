6ff82db7fefdea1449d43cd7694644e0ade5424b
Autumn <snqwfall@gmail.com>
2025-05-12 03:12:28 -0400

    made everything better (especially the approach function)

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,29 +1,31 @@
 extends living_entity # extends entity, extends CharacterBody2D
 
 var move_dir : int = 0 # player input axis
-var accel : int = 400 # ground acceleration, pixels/frame
-var air_accel : int = 400 # air acceleration, pixels/frame
-var friction : float = 0.2 # friction applied when not pressing input, ground
-var air_friction : float = 0.05 # friction applied when not pressing input, air
-var air_input_friction : float = 0.0002 # friction applied while input is pressed in air
-var input_friction : float = 0.001 # friction applied while input is pressed on ground
-var max_fall_speed : int = 1600 * 4 # max fall speed
-var max_walk_speed : int = 800 * 4 # max horizontal speed from walking
+@onready var movement_multiplier : int = 4
+
+@export var accel : int = 100 # ground acceleration, pixels/frame
+@export var air_accel : int = 100 # air acceleration, pixels/frame
+@export var friction : float = 0.2 # friction applied when not pressing input, ground
+@export var air_friction : float = 0.05 # friction applied when not pressing input, air
+@export var air_input_friction : float = 0.01 # friction applied while input is pressed in air
+@export var input_friction : float = 0.03 # friction applied while input is pressed on ground
+@export var max_fall_speed : int = 1600 # max fall speed
+@export var max_walk_speed : int = 800 # max horizontal speed from walking
 
 # Dash
-var dash_velocity : int = 2000 * 4 # speed of dash
+var dash_velocity : int = 2000 # speed of dash
 var dash_frames : int  = 12 # duration of dash in frames
 var special_dash_frames = 16 # extra dash frames (unused currently)
 var frames_since_dash : int # how many frames have passed since dash started
 var dash_direction : Vector2 # direction of dash
-var wavedash_vel : int = 3000 * 4 # velocity applied for wavedash
+var wavedash_vel : int = 2000 # velocity applied for wavedash
 var facing : Vector2 = Vector2(1,1) # direction the player is facing
 
 # Jump
 @export var jump_height : float = 300 * 4 # jump height
 @export var jump_seconds_to_peak : float = 0.5 # time to reach peak of jump
 @export var jump_seconds_to_descent : float = 0.4 # time from peak to ground
-@export var variable_jump_gravity_multiplier : float = 5 # gravity multiplier when jump is released early
+@export var variable_jump_gravity_multiplier : float = 10 # gravity multiplier when jump is released early
 @export var coyote_frames : int = 8 # time buffer after leaving ground to still allow jump
 @export var jump_buffer_frames : int = 4 # time buffer after pressing jump to allow jump
 var jump_buffer_time : int # counter for jump buffer
@@ -40,6 +42,14 @@ const CORNER_CORRECTION_HEIGHT = 20.0 # number of units allowed for vertical cor
 # Totem Abilities
 var afterimage_cast : bool = false # whether the afterimage is active
 var afterimage_pos : Vector2 # stored afterimage position
+
+func _ready() -> void:
+	accel *= movement_multiplier
+	air_accel *= movement_multiplier
+	max_fall_speed *= movement_multiplier
+	max_walk_speed *= movement_multiplier
+	dash_velocity *= movement_multiplier
+	wavedash_vel *= movement_multiplier
 
 func _physics_process(delta: float) -> void:
 	move(delta) # handles movement and player input
@@ -69,6 +79,13 @@ func _physics_process(delta: float) -> void:
 func animate():
 	$StaticSprite.scale.x = -facing.x # flip sprite based on facing
 
+func get_facing() -> Vector2:
+	if Input.get_axis("left","right") != 0:
+		facing.x = Input.get_axis("left","right")
+	if Input.get_axis("up","down") != 0:
+		facing.y = Input.get_axis("left","right")
+	return facing
+
 func move(delta):
 	move_dir = Input.get_axis("left", "right") # get input direction
 	var target_speed = move_dir * max_walk_speed # determine target speed
@@ -77,15 +94,8 @@ func move(delta):
 	if is_on_floor(): accel_rate = accel # use ground accel
 	else: accel_rate = air_accel # use air accel
 
-	if frames_since_dash == 0:
-		velocity.x = approach(velocity.x, target_speed, accel_rate * delta * 60) # move toward target speed
-
-func get_facing() -> Vector2:
-	if Input.get_axis("left","right") != 0:
-		facing.x = Input.get_axis("left","right")
-	if Input.get_axis("up","down") != 0:
-		facing.y = Input.get_axis("left","right")
-	return facing
+	if frames_since_dash == 0 and abs(velocity.x) <= max_walk_speed:
+		velocity.x = approach(velocity.x, target_speed, accel_rate * delta * 60)
 
 func approach(current: float, target: float, amount: float) -> float:
 	if current < target:
@@ -138,7 +148,7 @@ func jump():
 	if frames_since_dash > 0:
 		frames_since_dash = 0
 		velocity.y = jump_velocity / 1.5
-		velocity.x = wavedash_vel * move_dir
+		velocity.x = wavedash_vel * facing.x
 	else:
 		velocity.y = jump_velocity
 
