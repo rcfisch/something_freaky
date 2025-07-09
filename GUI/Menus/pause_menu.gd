6b4fce41d8463a565014fe5f1143b23940c6c3ac
Luxiah <141586861+Luxiah@users.noreply.github.com>
2025-07-08 23:21:41 -0400

    added basic options menu

--- GUI/Menus/pause_menu.gd
+++ GUI/Menus/pause_menu.gd
@@ -7,13 +7,18 @@ func _process(delta):
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		toggle_pause()
+		if $"../OptionsMenu".visible:
+			$"../OptionsMenu".hide()
+			show()
+		else:
+			toggle_pause()
 		
 		
 func toggle_pause():
 	globals.game_paused = !globals.game_paused
 	get_tree().paused = globals.game_paused
 	visible = globals.game_paused
+	
 
 
 func _on_resume_pressed():
@@ -22,3 +27,9 @@ func _on_resume_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_options_pressed():
+	hide()
+	$"../OptionsMenu".show()
+	
--- options.gd
+++ options.gd
@@ -1,4 +1,9 @@
-extends Node
+extends Control
 class_name options
 
 static var dash_keyboard_control_mode : bool = false # False=Mouse controlled, True=Keyboard controlled
+
+
+
+func _on_dash_keyboard_control_mode_pressed():
+	dash_keyboard_control_mode = !dash_keyboard_control_mode
